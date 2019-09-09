fe153e10e053866fc4fae0a5238ad3c0bef8244d
Peter Peill <peter.peill@yahoo.com>
2019-09-09 11:39:56 +0300

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/create'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/delete'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
