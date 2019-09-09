810646d49e9e7c0e512dcbcc56b31708f760a4eb
ian goldbaum <iangoldbaum@hebreo.cl>
2019-09-09 13:01:24 +0300

    carpet controller with new and create methods

---
+++ app/controllers/carpets_controller.rb
@@ -0,0 +1,20 @@
+class CarpetsController < ApplicationController
+  def new
+    @carpet = Carpet.new
+  end
+
+  def create
+    @carpet = Carpet.new(carpet_params)
+    if @carpet.save
+      redirect_to carpet_path(@carpet)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def carpet_params
+    params.require(:carpet).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'carpets/new'
+  get 'carpets/create'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+#   get 'about', to: 'pages#about', as: :about
+#   get 'contact', to: 'pages#contact', as: :contact
