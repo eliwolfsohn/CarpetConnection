86bfea1b8a303e83e247e96e9321cb6d12b5c543
Peter Peill <peter.peill@yahoo.com>
2019-09-11 13:53:29 +0300

    map,card

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 puts "emptying db ..."
 Booking.destroy_all
 Carpet.destroy_all
+  # remote_photo_url: "url"
 
 
 
@@ -17,33 +18,40 @@ Carpet.create!([{
   price: 5,
   passengers: 3,
   speed: 3900,
-  description: 'verz fast and very slow at the same time',
+  description: 'very fast and very slow at the same time',
   address: "32 Ben Yehuda, Tel Aviv",
-  user_id: 1
-},{
+  user_id: 1,
+  remote_photo_url: "https://cdn20.pamono.com/p/s/4/8/482920_f2r70mwwd2/antique-wool-carpet.jpg"
+ },{
   name: "CarBus",
   price: 50,
   passengers: 30,
   speed: 500,
   description: 'FUn RIde',
   address: "Jerusalem",
-  user_id: 1
+  user_id: 1,
+  remote_photo_url: "https://cdn20.pamono.com/p/s/4/8/482920_f2r70mwwd2/antique-wool-carpet.jpg"
+
 },{
-  name: "Hassan",
+  name: "Mcdoof",
   price: 1,
   passengers: 1,
   speed: 1000,
   description: 'things are like this for a reason',
   address: "Munich",
-  user_id: 1
+  user_id: 1,
+  remote_photo_url: "https://cdn20.pamono.com/p/s/4/8/482920_f2r70mwwd2/antique-wool-carpet.jpg"
+
 },{
-  name: "The Peter Experience",
+  name: "The Happy Experience",
   price: 100,
   passengers: 5,
   speed: 4000,
   description: 'better wear goggles',
   address: "San Fransisco",
-  user_id: 1
+  user_id: 1,
+  remote_photo_url: "https://cdn20.pamono.com/p/s/4/8/482920_f2r70mwwd2/antique-wool-carpet.jpg"
+
 },{
   name: "Whoops look over there",
   price: 100,
@@ -51,7 +59,9 @@ Carpet.create!([{
   speed: 4000,
   description: 'bring a helmet',
   address: "Tel Aviv",
-  user_id: 1
+  user_id: 1,
+  remote_photo_url: "https://cdn20.pamono.com/p/s/4/8/482920_f2r70mwwd2/antique-wool-carpet.jpg"
+
 },{
   name: "Cookie Monster",
   price: 100,
@@ -59,7 +69,9 @@ Carpet.create!([{
   speed: 4000,
   description: 'eat me',
   address: "Bombay",
-  user_id: 1
+  user_id: 1,
+  remote_photo_url: "https://cdn20.pamono.com/p/s/4/8/482920_f2r70mwwd2/antique-wool-carpet.jpg"
+
 },{
   name: "General Ralala",
   price: 100,
@@ -67,7 +79,9 @@ Carpet.create!([{
   speed: 4000,
   description: 'Get me a beer',
   address: "Moscow",
-  user_id: 1
+  user_id: 1,
+  remote_photo_url: "https://cdn20.pamono.com/p/s/4/8/482920_f2r70mwwd2/antique-wool-carpet.jpg"
+
 },{
   name: "For those about to rock",
   price: 100,
@@ -75,7 +89,9 @@ Carpet.create!([{
   speed: 4000,
   description: 'we salute you',
   address: "Rome",
-  user_id: 1
+  user_id: 1,
+  remote_photo_url: "https://cdn20.pamono.com/p/s/4/8/482920_f2r70mwwd2/antique-wool-carpet.jpg"
+
 },{
   name: "Ian rulez",
   price: 100,
@@ -83,7 +99,39 @@ Carpet.create!([{
   speed: 4000,
   description: 'ya tu sabes',
   address: "Chile",
-  user_id: 1
+  user_id: 1,
+  remote_photo_url: "https://cdn20.pamono.com/p/s/4/8/482920_f2r70mwwd2/antique-wool-carpet.jpg"
+
+},{
+  name: "General Falafel Gabay",
+  price: 100,
+  passengers: 5,
+  speed: 4000,
+  description: 'Get me a beer',
+  address: "Moscow",
+  user_id: 1,
+  remote_photo_url: "https://cdn20.pamono.com/p/s/4/8/482920_f2r70mwwd2/antique-wool-carpet.jpg"
+
+},{
+  name: "Bens Rug",
+  price: 1,
+  passengers: 1000,
+  speed: 1,
+  description: 'Better Hurry UP',
+  address: "Manila",
+  user_id: 1,
+  remote_photo_url: "https://cdn20.pamono.com/p/s/4/8/482920_f2r70mwwd2/antique-wool-carpet.jpg"
+
+},{
+  name: "Ruggedly Ruggy",
+  price: 127,
+  passengers: 8,
+  speed: 2000,
+  description: 'Come and feel the fibre of my fabric',
+  address: "Novosibirsk",
+  user_id: 1,
+  remote_photo_url: "https://cdn20.pamono.com/p/s/4/8/482920_f2r70mwwd2/antique-wool-carpet.jpg"
+
 }
 ])
 puts 'seeding concluded'
